b21fbfdaa5a0476da08d0426c929ab2e8426829f
呂浩宇 <y26704325@gmail.com>
2018-03-27 18:02:17 +0800

    new route

--- main.go
+++ main.go
@@ -1,17 +1,13 @@
-// @APIVersion 1.0.0
-// @APITitle Teamwork Desk
-// @APIDescription Bend Teamwork Desk to your will using these read and write endpoints
-// @Contact support@teamwork.com
-// @TermsOfServiceUrl https://www.teamwork.com/termsofservice
-// @License BSD
-// @LicenseUrl http://opensource.org/licenses/BSD-2-Clause
-
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"strings"
 )
 
+//IP:
 func main() {
 	http.HandleFunc("/swagger.json", swagger)
 	changeHeaderThenServe := func(h http.Handler) http.HandlerFunc {
@@ -27,6 +23,10 @@ func main() {
 	// http.Handle("/docker/", http.StripPrefix("/docker", http.FileServer(http.Dir("/var/docker/volume/swagger"))))
 	// http.Handle("/docker/", changeHeaderThenServe(http.StripPrefix("/docker", http.FileServer(http.Dir("C:\\docker\\gitlab\\volume\\swagger")))))
 	// http.Handle("/docker2/", http.StripPrefix("/docker2", http.FileServer(http.Dir("C:\\docker\\gitlab\\volume\\swagger"))))
+
+	//Gitlab Webhooks
+	http.HandleFunc("/gitlab", gitlabMergeEvent)
+
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -35,4 +35,34 @@ func swagger(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 	http.ServeFile(w, req, "swagger.json")
+}
+
+type test_struct struct {
+	Test string
+}
+
+type Data struct {
+	Name  string
+	Value string
+}
+
+func gitlabMergeEvent(w http.ResponseWriter, req *http.Request) {
+	if err := req.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+
+	total := []*Data{}
+
+	for k, v := range req.Form {
+		item := &Data{}
+		item.Name = k
+		item.Value = strings.Join(v, "")
+
+		total = append(total, item)
+	}
+
+	if err := json.NewEncoder(w).Encode(total); err != nil {
+		panic(err)
+	}
 }
